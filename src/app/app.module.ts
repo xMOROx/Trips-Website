222267760d99aa38d5676d2d9d130b9544c02e6f
xMOROx <zajdel0202@gmail.com>
2022-12-02 19:20:48 +0100

    Configure google map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,35 +4,37 @@ import { TripsModule } from './trips/trips.module';
 import { AppRoutingModule } from './app-routing.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FormsModule } from '@angular/forms';
+import { GoogleMapsModule } from '@angular/google-maps'
 
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { TripsCartComponent } from './trips-cart/trips-cart.component';
+import { PageNotFoundComponent } from './pageNotFound/pageNotFound.component';
+import { FooterComponent } from './footer/footer.component';
 
 import { CartService } from './services/cart.service';
 import { CurrenciesService } from './services/currencies.service';
 import { FiltersService } from './services/filters.service';
-import { PageNotFoundComponent } from './pageNotFound/pageNotFound.component';
-import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
-  declarations: [	
+  declarations: [
     AppComponent,
     NavBarComponent,
     HomePageComponent,
     TripsCartComponent,
     PageNotFoundComponent,
-      FooterComponent
-   ],
+    FooterComponent
+  ],
   imports: [
     BrowserModule,
     TripsModule,
     AppRoutingModule,
     FontAwesomeModule,
-    FormsModule
+    FormsModule,
+    GoogleMapsModule
   ],
   providers: [
     CartService,
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,14 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { faBars, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { MapInfoWindow, MapMarker } from '@angular/google-maps';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
-
+  @ViewChild(MapInfoWindow, { static: false }) info!: MapInfoWindow
   public faBars: IconDefinition = faBars;
+
+  public zoom = 13;
+  public center!: google.maps.LatLngLiteral;
+  public options: google.maps.MapOptions = {
+    mapTypeId: 'terrain',
+    draggable: false,
+    zoomControl: false,
+    scrollwheel: false,
+    streetViewControl: false,
+    rotateControl: false,
+    fullscreenControl: false,
+    mapTypeControl: false,
+    disableDoubleClickZoom: true,
+  };
+
+  public optionsMarker: any = { animation: google.maps.Animation.DROP }
 
   public constructor(private titleService: Title) { }
 
@@ -20,7 +37,14 @@ export class HomePageComponent implements OnInit {
 
   ngOnInit(): void {
     this.setTitle("Strona Główna");
+    this.center = {
+      lat: 33.7866,
+      lng: -118.2987
+    }
   }
 
+  public openInfo(marker: MapMarker) {
+    this.info.open(marker);
+  }
 
 }
