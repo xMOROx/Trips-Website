96b89ed823a5265436e5432fedaac795c00497a4
xMOROx <zajdel0202@gmail.com>
2022-12-14 21:14:06 +0100

    Allow only for admin

--- src/app/pageSettings/pageSettings.component.ts
+++ src/app/pageSettings/pageSettings.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Currencies } from '../Models/Currencies.enum';
+import { SessionOptions } from '../Models/sessionOptions.enum';
 import { SettingsChangeService } from '../services/settingsChange.service';
 
 @Component({
@@ -10,19 +11,28 @@ import { SettingsChangeService } from '../services/settingsChange.service';
 })
 export class PageSettingsComponent implements OnInit {
   public currenciesArray: string[] = [];
+  public persistanceArray: string[] = [];
   public selectedCurrency!: string;
+  public selectedPersistance!: string;
   constructor(private titleService: Title, private settings: SettingsChangeService) { }
 
   ngOnInit() {
     this.titleService.setTitle("Ustawienia");
     this.currenciesArray = Object.values(Currencies);
-    this.settings.getCurrency().subscribe((currency: string) => {
-      this.selectedCurrency = currency;
+    this.persistanceArray = Object.values(SessionOptions);
+    this.settings.getCurrency().subscribe((currency: any) => {
+      this.selectedCurrency = currency.value;
+    });
+    this.settings.getPersistance().subscribe((persistance: any) => {
+      this.selectedPersistance = persistance.value;
     });
   }
 
-  public onCurrencyChange(event: any) {
-    this.settings.changeCurrency(this.selectedCurrency);
+  public onCurrencyChange(_: any) {
+    this.settings.changeValueByKey('currency', { value: this.selectedCurrency });
+  }
+  public onPersistanceChange(_: any) {
+    this.settings.changeValueByKey('persistance', { value: this.selectedPersistance });
   }
 }
 
