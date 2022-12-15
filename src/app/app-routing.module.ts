b5faa1a54d7f5e2577fd027679c8237793834c75
xMOROx <zajdel0202@gmail.com>
2022-12-15 15:07:14 +0100

    Add new routing to admin and management panel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
-import { HomePageComponent } from './home-page/home-page.component';
-import { ListTripsComponent } from './trips/list-trips/list-trips.component';
+import { HomePageComponent } from './layout/home-page/home-page.component';
+import { ListTripsComponent } from './trips/trip-display/list-trips/list-trips.component';
 import { ManageTripsComponent } from './trips/manage-trips/manage-trips.component';
 import { TripsCartComponent } from './trips-cart/trips-cart.component';
 import { PageNotFoundComponent } from './pageNotFound/pageNotFound.component';
 import { SingleTripComponent } from './trips/singleTrip/singleTrip.component';
 import { BuyHistoryComponent } from './buyHistory/buyHistory.component';
-import { PageSettingsComponent } from './pageSettings/pageSettings.component';
+import { PageSettingsComponent } from './page-administration/pageSettings/pageSettings.component';
 import { SignInComponent } from './authentication/sign-in/sign-in.component';
 import { SignUpComponent } from './authentication/sign-up/sign-up.component';
 import { DashboardComponent } from './authentication/dashboard/dashboard.component';
@@ -18,10 +18,12 @@ import { VerifyEmailComponent } from './authentication/verify-email/verify-email
 import { AuthGuard } from './authentication/guard/auth.guard';
 import { AdminGuard } from './authentication/guard/admin.guard';
 import { CanReadGuard } from './authentication/guard/can-read.guard';
+import { AdminPanelComponent } from './page-administration/admin-panel/admin-panel.component';
 
 const routes: Routes = [
   { path: 'cart', component: TripsCartComponent, canActivate: [AuthGuard, CanReadGuard] },
   { path: 'manage', component: ManageTripsComponent, canActivate: [AuthGuard, AdminGuard] },
+  { path: 'admin-panel', component: AdminPanelComponent, canActivate: [AuthGuard, AdminGuard] },
   { path: 'trips', component: ListTripsComponent },
   { path: 'trips/:key', component: SingleTripComponent, canActivate: [AuthGuard, CanReadGuard] },
   { path: 'buyHistory', component: BuyHistoryComponent, canActivate: [AuthGuard, CanReadGuard] },
